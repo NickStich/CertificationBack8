7f5b7fc71bd3561a2e7a7e69cce16abdf67fcdb0
Nick <nickstich@icloud.com>
2020-07-23 22:16:08 +0300

    Implements GET and POST methods

--- src/main/java/com/example/cert/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.cert;
-
-import com.example.cert.model.Certification;
-
-public class Main {
-    public static void main(String[] args) {
-        Certification certification = new Certification();
-    }
-}
